fac20b1254c5ef2fa2f4181b5efb019f1692141d
SunnY <nakerin7588@gmail.com>
2024-10-13 06:46:08 +0700

    Update all answer

--- FRA333_HW3_6533_6573.py
+++ FRA333_HW3_6533_6573.py
@@ -5,18 +5,124 @@
 1.นครินทร์_6533
 2.พีรดา_6573
 '''
-#=============================================<คำตอบข้อ 1>======================================================#
+#=============================================<Import library>==================================================#
+from HW3_utils import FKHW3
+import numpy as np
+#==========================================<Initiallize variables>==============================================#
+
+#===============================================<คำตอบข้อ 1>=====================================================#
+'''
+
+'''
 #code here
 def endEffectorJacobianHW3(q:list[float])->list[float]:
-    pass
-#==============================================================================================================#
+    # Get q1,q2 and q3 from argument "q"
+    [q1,q2,q3] = q
+    
+    # Get R, P, R_e, p_e from FKHW3(q)
+    R, P, R_e, p_e = FKHW3(q)
+    
+    # Define position vector from FKHW3(q)
+    p_0_1 = np.array(P[:,0]).reshape(3,1)  # Position vector from frame 0 to frame 1
+    p_0_2 = np.array(P[:,1]).reshape(3,1)  # Position vector from frame 0 to frame 2
+    p_0_3 = np.array(P[:,2]).reshape(3,1)  # Position vector from frame 0 to frame 3
+    p_0_e = np.array(P[:,3]).reshape(3,1)  # Position vector from frame 0 to frame e
+    
+    # Define rotation axis variables
+    '''
+        z_1 , z_2 , z_3 are from projection of z at frame 1, 2, 3 to frame 0
+        ** You can see how we calculate at calculationpaper.pdf
+    '''
+    z_1 = np.array([[0], [0], [1]])                    # Rotation axis at joint 1
+    z_2 = np.array([[np.sin(q1)], [np.cos(q1)], [0]])  # Rotation axis at joint 2
+    z_3 = np.array([[np.sin(q1)], [np.cos(q1)], [0]])  # Rotation axis at joint 3
+    
+    '''
+        Jacobian calculation
+            J_i = [Jv_i(q) Jw_i(q)]^T
+            Jv(q)|Jw(q) = [z_i 0]^T if joint i is Prismatic joint
+            Jv(q)|Jw(q) = [z_ix(P_0_e - P_0_i) 0]^T if joint i is Revolute joint
+            Let ^T is transpose
+            ** You can see how we calculate at calculationpaper.pdf
+    '''
+    try:
+        # Compute the cross product of z and p using np.cross
+        cross_1 = np.cross(z_1.flatten(), (p_0_e - p_0_1).flatten())
+        cross_2 = np.cross(z_2.flatten(), (p_0_e - p_0_2).flatten())
+        cross_3 = np.cross(z_3.flatten(), (p_0_e - p_0_3).flatten())
+    except Exception as e:
+        print(f"Error calculating cross product: {e}")
+        return np.zeros((3, 6))
+        
+    # Reshape the cross product from 1x3 to 3x1
+    cross_1 = cross_1.reshape(3, 1)
+    cross_2 = cross_2.reshape(3, 1)
+    cross_3 = cross_3.reshape(3, 1)
+    
+    # Concatenate the cross product at vertical axis
+    column_1 = np.concatenate((cross_1, z_1), axis = 0)
+    column_2 = np.concatenate((cross_2, z_2), axis = 0) 
+    column_3 = np.concatenate((cross_3, z_3), axis = 0) 
+    
+    # Stack the cross product at horizontal axis
+    jacobian = np.hstack((column_1, column_2, column_3))
+    
+    return jacobian
+#===============================================================================================================#
 #=============================================<คำตอบข้อ 2>======================================================#
 #code here
 def checkSingularityHW3(q:list[float])->bool:
-    pass
+    '''
+        Check singularity
+            Singularity is happend when det(J) is 0
+            But we can calculate det if matrix is square. So we need to simplify the matrix to be square.
+            From robot model end effector can only translate in x, y, z direction but can not rotate. So we will reduce the matrix by removing the Angular Jacobian.
+            
+            J_i = [Jv_i(q)] ; Remove the Angular Jacobian
+            
+            ** In computer programming or real world when we calculate Det(J) and it near to zero with small tolerance we can let's it be singularity
+    '''
+    try:
+        det_J = np.linalg.det(endEffectorJacobianHW3(q)[0:3, :])
+        print(f"determinant of Jacobian from checkSingularityHW3 function is {det_J}")
+    except Exception as e:
+        print(f"Error calculating det: {e}")
+        return False
+    tolerance = 1e-3
+    
+    return np.isclose(det_J, 0.0, atol=tolerance).all()
 #==============================================================================================================#
 #=============================================<คำตอบข้อ 3>======================================================#
 #code here
 def computeEffortHW3(q:list[float], w:list[float])->list[float]:
-    pass
-#==============================================================================================================#
+    # Get R, P, R_e, p_e from FKHW3(q)
+    R, P, R_e, p_e = FKHW3(q)
+    
+    # w is a list of [forces , moments]
+    F_e = np.array(w)[0:3].reshape(3, 1)
+    M_e = np.array(w)[3:6].reshape(3, 1)
+    
+    # Rereference frame for calculate tau because Jacobian is reference from frame 0
+    '''
+        Rereferences frame
+            from w_0 = R_e @ w_e get:
+            F_0 = R_e @ F_e
+            M_0 = R_e @ M_e
+    '''
+    F_0 = R_e @ F_e
+    M_0 = R_e @ M_e
+    
+    # Create new w
+    w = np.vstack((F_0, M_0))
+    
+    # Calculate the tau
+    '''
+        tau calculation
+            tau = Jacobian(q)^T @ w
+            **  w must be reference same as the jacobian like if jacobian is reference from frame 0 w must be reference from frame 0
+                if it not from the same frame we need to change frame of w to reference frame same as jacobian
+    '''
+    tau = endEffectorJacobianHW3(q).T @ w
+    
+    return tau
+#==============================================================================================================
--- testScript.py
+++ testScript.py
@@ -5,15 +5,87 @@
 1.นครินทร์_6533
 2.พีรดา_6573
 '''
-#===========================================<ตรวจคำตอบข้อ 1>====================================================#
-#code here
 
+#=============================================<Import library>==================================================#
+from HW3_utils import FKHW3
+from FRA333_HW3_6533_6573 import endEffectorJacobianHW3, checkSingularityHW3, computeEffortHW3
+
+import roboticstoolbox as rtb
+from spatialmath.base import *
+from spatialmath import SE3, SO3
+import numpy as np
+#=========================<Create robot from roboticstoolbox for check answer>=================================#
+d1, a2 = 0.0892, 0.425
+mdh = [[0, 0, d1, np.pi], [0, np.pi/2.0, 0, 0], [-a2, 0, 0, 0]]
+revjoint = [] # Create revolute joint
+for data in mdh:
+    revjoint.append(rtb.RevoluteMDH(a=data[0], alpha=data[1], d=data[2], offset=data[3])) # Append revolute joint
+
+# Get Transformation Matrix from FKHW3 function
+R,P,R_e,p_e = FKHW3([0, 0 ,0]) # Home configuration
+T_0_3 = np.hstack((R[:, :, 2], P[:, 2].reshape(3, 1)))
+T_0_3 = np.vstack((T_0_3, [0, 0, 0, 1]))
+T_0_3 = SE3(T_0_3)
+
+T_0_e = np.hstack((R[:, :, 3], P[:, 3].reshape(3, 1)))
+T_0_e = np.vstack((T_0_e, [0, 0, 0, 1]))
+T_0_e = SE3(T_0_e)
+
+T_3_e = T_0_3.inv() @ T_0_e
+
+# Create robot model from MDH-Parameter 
+robot = rtb.DHRobot(
+    [
+        revjoint[0],
+        revjoint[1],
+        revjoint[2]
+    ],tool = T_3_e, name="3R robot"
+    )
+
+#=====================================<Define important variables>==============================================#
+q = [0, 0, 0] # Configuration space
+w = [1, 2, 3, 4, 5, 6] # Force and Moment that reference at frame e [force moment]
+#===========================================<ตรวจคำตอบข้อ 1>====================================================#
+print("================================")
+print("Question 1 start answer checking")
+jacobian = endEffectorJacobianHW3(q) # Calculate Jacobian from endEffectorJacobianHW3
+
+# Check answer
+print("Answer from endEffectorJacobianHW3")
+print(jacobian)
+print("Answer from RoboticsToolbox")
+print(robot.jacob0(q = q)) # Get Jacobian from roboticsToolbox
+# Use np.isclose to check that the Jacobian from endEffectorJacobianHW3 and the Jacobian from roboticsToolbox are the same with low tolerance
+print(f"Answer from np.isclose is {np.isclose(jacobian, robot.jacob0(q = q), atol=1e-3).all()}")
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 2>====================================================#
-#code here
-
+print("================================")
+print("Question 2 start answer checking")
+# Check answer
+print("Answer from endEffectorJacobianHW3")
+flag = checkSingularityHW3(q) # Check singularity from checkSingularityHW3
+print(flag)
+print("Answer from RoboticsToolbox")
+print(f"determinant of Jacobian from checkSingularityHW3 function is {np.linalg.det(robot.jacob0(q = q)[0:3, :])}")
+print(np.isclose(np.linalg.det(robot.jacob0(q = q)[0:3, :]), 0.0, atol=1e-3).all()) # Get Jacobian from roboticsToolbox and reduce it to be square
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 3>====================================================#
-#code here
+print("================================")
+print("Question 3 start answer checking")
+tau = computeEffortHW3(q, w)
 
+# Check answer
+print("Answer from computeEffortHW3")
+print(tau)
+print("Answer from RoboticsToolbox")
+print("Check answer from jacobian that reference from frame 0")
+w_reframe = np.vstack((R_e @ np.array(w)[0:3].reshape(3, 1), R_e @ np.array(w)[3:6].reshape(3, 1))) # w that has rereference frame to frame 0 same as jacobian
+tau_jacob0 = robot.jacob0(q = q).T @ w_reframe # Calculate tau using jacobian from roboticsToolbox
+print(tau_jacob0)
+print("Check answer from jacobian that reference from frame e")
+tau_jacobe = robot.jacobe(q = q).T @ np.array(w).reshape(6,1)
+print(tau_jacobe) # Calculate tau using jacobian from roboticsToolbox
+# Use np.isclose to check that the tau from computeEffortHW3 and the computeEffortHW3 from roboticsToolbox are the same with low tolerance
+print(f"Answer from np.isclose(RTB reference jacobian from frame 0) is {np.isclose(tau, tau_jacob0, atol=1e-3).all()}")
+print(f"Answer from np.isclose(RTB reference jacobian from frame e) is {np.isclose(tau, tau_jacobe, atol=1e-3).all()}")
 #==============================================================================================================#
